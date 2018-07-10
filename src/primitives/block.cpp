93e3e0522700fd46ca716fa1c0f291ca4889b27f
ahmedbodi <ahmed_bodi@msn.com>
2018-07-10 23:52:02 +0000

    Swap Block Hash to X11

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -14,7 +14,7 @@
 
 uint256 CBlockHeader::GetHash() const
 {
-    return HashQuark(BEGIN(nVersion), END(nNonce));
+    return HashX11(BEGIN(nVersion), END(nNonce));
 }
 
 uint256 CBlock::BuildMerkleTree(bool* fMutated) const
